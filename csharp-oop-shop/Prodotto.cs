e11bd80208ab7f31b517c50b7d9c6cb0da01a18a
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-08 14:30:36 +0200

    settata la classe prodotto con proprio costruttore, getter, setter e metodi

---
+++ csharp-oop-shop/Prodotto.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csharp_oop_shop
+{
+    internal class Prodotto
+    {
+        // ATTRIBUTI con eventuali GETTER e SETTER
+        private int codice;
+
+        public int Codice
+        {
+            get;
+        }
+
+        private string nome;
+
+        public string Nome
+        {
+            get; set;
+        }
+
+        private string descrizione;
+        public string Descrizione
+        {
+            get; set;
+        }
+
+        private float prezzo;
+        public float Prezzo
+        {
+            get; set;
+        }
+
+        private int iva;
+        public int Iva
+        {
+            get; set;
+        }
+
+        // COSTRUTTORI
+        public Prodotto(string nome, string descrizione, float prezzo, int iva)
+        {
+            this.codice = GeneraCodiceRandom();
+            this.nome = nome;   
+            this.descrizione = descrizione;
+            this.prezzo = prezzo;
+            this.iva = iva;
+        }
+
+        // METODI
+        // funzione che genera un numero da 1000 a 9999
+        private int GeneraCodiceRandom()
+        {
+            Random randomNumber = new Random();
+            return randomNumber.Next(1000, 10000);
+        }
+        // funzione che visualizza il prezzo base
+        public string VisualizzaPrezzoBase()
+        {
+            return this.Prezzo + " €";
+        }
+        // funzione che visualizza il prezzo base aggiunto di iva
+        public string VisualizzaPrezzoPiuIva()
+        {
+            float prezzoPiuIva = (this.Prezzo * this.Iva) / 100;
+            return prezzoPiuIva + " €";
+        }
+        // funzione che visualizza il nome esteso
+        public string VisualizzaNomeEsteso()
+        {
+            return this.Codice + " " + this.Nome;
+        }
+    }
+}
