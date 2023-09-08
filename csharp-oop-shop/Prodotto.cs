da5df12d40eab33d0b000bd8761949395c4d785f
Marco Rinaldi <marcorinaldi98@libero.it>
2023-09-08 16:28:33 +0200

    bonus completati

--- csharp-oop-shop/Prodotto.cs
+++ csharp-oop-shop/Prodotto.cs
@@ -9,57 +9,58 @@ namespace csharp_oop_shop
     internal class Prodotto
     {
         // ATTRIBUTI con eventuali GETTER e SETTER
-        private int codice;
 
-        public int Codice { get; }
-
-        private string nome;
+        public string Codice { get; }
 
         public string Nome { get; set; }
-
-        private string descrizione;
         public string Descrizione { get; set; }
 
-        private double prezzo;
-
         public double Prezzo { get; set; }
-
-        private int iva;
 
         public int Iva { get; set; }
 
         // COSTRUTTORI
         public Prodotto(string nome, string descrizione, double prezzo, int iva)
         {
-            this.codice = GeneraCodiceRandom();
-            this.nome = nome;   
-            this.descrizione = descrizione;
-            this.prezzo = prezzo;
-            this.iva = iva;
+            int codiceGenerato = GeneraCodiceRandom();
+            this.Codice = codiceGenerato.ToString("D8"); //genero un pad left di 0 per arrivare a 8 caratteri
+            this.Nome = nome;   
+            this.Descrizione = descrizione;
+            this.Prezzo = prezzo;
+            this.Iva = iva;
         }
 
         // METODI
-        // funzione che genera un numero da 1000 a 9999
+        // funzione che genera un numero da 100 a 99999999
         private int GeneraCodiceRandom()
         {
             Random randomNumber = new Random();
-            return randomNumber.Next(1000, 10000);
+            return randomNumber.Next(1, 100000000);
         }
         // funzione che visualizza il prezzo base
         public string VisualizzaPrezzoBase()
         {
-            return this.Prezzo + " €";
+            return this.Prezzo.ToString("0.00") + " €";
         }
         // funzione che visualizza il prezzo base aggiunto di iva
         public string VisualizzaPrezzoPiuIva()
         {
             double calcoloIva = (this.Prezzo * this.Iva) / 100;
-            return (this.Prezzo + calcoloIva) + " €";
+            return (this.Prezzo + calcoloIva).ToString("0.00") + " €";
         }
         // funzione che visualizza il nome esteso
         public string VisualizzaNomeEsteso()
         {
-            return this.Codice + " " + this.Nome;
+            return "#" + this.Codice + " " + this.Nome;
+        }
+
+        // funzione che stampoa tutti i dettagli di un Prodotto
+        public void StampaProdotto()
+        {
+            Console.WriteLine($"Nome: {this.Nome}");
+            Console.WriteLine($"Codice: #{this.Codice}");
+            Console.WriteLine($"Prezzo: {this.VisualizzaPrezzoPiuIva()}");
+            Console.WriteLine($"Descrizione: {this.Descrizione};");
         }
     }
 }
--- csharp-oop-shop/Program.cs
+++ csharp-oop-shop/Program.cs
@@ -1,24 +1,47 @@
 ﻿using csharp_oop_shop;
 
-Prodotto telefono = new Prodotto("iPhone 14", "Telecono cellulare di casa Apple", 850.99f, 22);
+Console.OutputEncoding = System.Text.Encoding.UTF8; // per far leggere alla console simboli speciali come il simbolo dell'euro
+
+Prodotto iPhone = new Prodotto("iPhone 14", "Telefono cellulare di casa Apple", 850.99d, 22);
 
 
-Console.WriteLine($"Codice: #{telefono.Codice}");
-Console.WriteLine($"Nome: {telefono.Nome}");
-Console.WriteLine($"Descrizione: {telefono.Descrizione}");
-Console.WriteLine($"Prezzo Base: {telefono.Prezzo}€");
-Console.WriteLine($"IVA: {telefono.Iva}%");
-
-Console.WriteLine();
-
-Console.WriteLine($"Nome esteso: {telefono.VisualizzaNomeEsteso}");
-Console.WriteLine($"Prezzo Base: {telefono.VisualizzaPrezzoBase}");
-Console.WriteLine($"Prezzo compreso di IVA: {telefono.VisualizzaPrezzoPiuIva}");
+Console.WriteLine($"Codice: #{iPhone.Codice}");
+Console.WriteLine($"Nome: {iPhone.Nome}");
+Console.WriteLine($"Descrizione: {iPhone.Descrizione}");
+Console.WriteLine($"Prezzo Base: {iPhone.Prezzo.ToString("0.00")} €");
+Console.WriteLine($"IVA: {iPhone.Iva}%");
 
 Console.WriteLine();
 
-telefono.Nome = "Nuovo nome";
-telefono.Descrizione = "Nuova descrizione";
+Console.WriteLine($"Nome esteso: {iPhone.VisualizzaNomeEsteso()}");
+Console.WriteLine($"Prezzo Base: {iPhone.VisualizzaPrezzoBase()}");
+Console.WriteLine($"Prezzo compreso di IVA: {iPhone.VisualizzaPrezzoPiuIva()}");
 
-Console.WriteLine($"Nome modificato: {telefono.Nome}");
-Console.WriteLine($"Descrizione modificata: {telefono.Descrizione}");
+Console.WriteLine();
+
+iPhone.Nome = "iPhone 15";
+iPhone.Descrizione = "Nuovo modello di cellulare della Apple";
+
+Console.WriteLine($"Nome modificato: {iPhone.Nome}");
+Console.WriteLine($"Descrizione modificata: {iPhone.Descrizione}");
+
+Console.WriteLine(Environment.NewLine);
+
+// BONUS
+//Usando un array fisso di 5 prodotti, dichiarate un elenco dei prodotti di un negozio e inseriteci al suo interno qualche prodotto che vi aspettate di trovare nel negozio. Stampate poi l’elenco dei vostri prodotti che avete previsto nel negozio.
+Prodotto iPad = new Prodotto("iPad", "Tablet di casa Apple", 400.00d, 22);
+Prodotto macBook = new Prodotto("MacBook", "Computer portatile della Apple", 1100.00d, 22);
+Prodotto iPod = new Prodotto("iPod", "Lettore di musica della Apple", 200.00d, 22);
+Prodotto watch = new Prodotto("Apple Watch", "Orologio touch screen leggero", 300.00d, 22);
+
+// Creazione di un array di prodotti
+Prodotto[] apple = { iPhone, iPad, macBook, iPod, watch };
+
+// Stampa dell'elenco dei prodotti nel negozio
+Console.WriteLine("Il negozio Apple ha a disposizione questi prodotti:");
+for (int i = 0; i < apple.Length; i++)
+{
+    Console.WriteLine();
+    Console.WriteLine($"- Prodotto {i + 1}:");
+    apple[i].StampaProdotto();
+}
